030d67f4d501c2bc0dd7b29164b962b0f7c8468a
shravan-balasubramanian <shravan.balasubramanian@freshworks.com>
2020-12-22 12:41:57 +0530

    change url

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-const BASE_URL = 'https://sample-nodejs-4u8rr.ondigitalocean.app/';
+const BASE_URL = 'https://sample-nodejs-o5f4g.ondigitalocean.app/';
 
 export default {
   name: 'digitalOceanApp',
